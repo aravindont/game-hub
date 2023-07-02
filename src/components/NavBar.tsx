06010f4dbb6fc94e265cf4bb4f752fc274a02e79
Aravind104 <ontagodiaravind@gmail.com>
2023-07-02 22:42:41 +0530

    Build a responsive layout

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+const NavBar = () => {
+  return (
+    <div>
+      <HStack>
+        <Image src={logo} />
+      </HStack>
+    </div>
+  );
+};
+
+export default NavBar;
